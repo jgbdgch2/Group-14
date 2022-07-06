f976143920c10cb077f81337dc8b77d0fa2a426a
jgbdgch2 <68649427+jgbdgch2@users.noreply.github.com>
2022-07-05 20:20:01 -0400

    Added text detection
    Text detection might help find things idk

--- measurement_marker_detector.py
+++ measurement_marker_detector.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import opencv_text_detection_image as text_detect
 
 def measure_line(points):
     x1, y1, x2, y2 = points
@@ -32,9 +33,10 @@ def find_measurement_marker(file_name):
                 minLineLength=5, # Min allowed length of line
                 maxLineGap=10 # Max allowed gap between line for joining them
                 )
-                
-    print(lines)
 
+    detected_text, rW, rH = text_detect.find_text(image)
+    print(detected_text, rW, rH)
+    
     max_line = (0, 0, 0, 0)
      
     # Iterate over points
@@ -50,8 +52,23 @@ def find_measurement_marker(file_name):
         
         #print(((x1-x2)**2 + (y1-y2)**2)**(1/2))
         max_line = compare_lines(points[0], max_line)
+        
+        
+        
+    for (startX, startY, endX, endY) in detected_text:
+        # scale the bounding box coordinates based on the respective
+        # ratios
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+
+        # draw the bounding box on the image
+        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
          
     # Save the result image
     cv2.imwrite('detectedLines.png',image)
 
     return max_line
+
+find_measurement_marker('derp.png')
